4087a67c3c3f34c624aeabc67da611cc1a52af5c
Viktor Schastnyy <victor92sh@gmail.com>
2021-02-27 11:02:31 +0300

    Added expose decorator to cryatlon data dto

--- src/models/cryatlons/dto/cryatlon.dto.ts
+++ src/models/cryatlons/dto/cryatlon.dto.ts
@@ -1,38 +1,47 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsDate, IsEnum, IsInt, IsNotEmpty, IsString } from 'class-validator';
 import { Gender } from '@common/enums/gender.enum';
+import { Expose } from 'class-transformer';
 
 export class GetCryatlonDataDTO {
   @ApiProperty()
+  @Expose()
   @IsInt()
   id: number;
 
   @ApiProperty()
+  @Expose()
   @IsString()
   @IsNotEmpty()
   name: string;
 
   @ApiProperty()
+  @Expose()
   @IsInt()
   run_distance: number;
 
   @ApiProperty()
+  @Expose()
   @IsInt()
   ski_distance: number;
 
   @ApiProperty()
+  @Expose()
   @IsInt()
   water_distance: number;
 
   @ApiProperty()
+  @Expose()
   @IsInt()
   barefoot_distance: number;
 
   @ApiProperty()
+  @Expose()
   @IsEnum(Gender)
   gender: Gender;
 
   @ApiProperty()
+  @Expose()
   @IsDate()
   date: Date;
 }
